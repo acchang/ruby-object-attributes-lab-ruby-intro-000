dbabe0b42e4351ece2280c6c1b5c35dd7902149c
Andrew C <acchang4@gmail.com>
2020-04-18 18:45:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
 class Dog
 
-  def initialize(name)
-    @name = name
-  end
 
-  def name
-    @name
-  end 
-end
+Dog.instance_variable_set(:@name)
+Dog.instance_variable_get(:@name)
+
+
+#   def initialize(name)
+#     @name = name
+#   end
+# 
+#   def name
+#     @name
+#   end 
+# end
 
 # def name=(dog_name)
 #   @this_dogs_name = dog_name
