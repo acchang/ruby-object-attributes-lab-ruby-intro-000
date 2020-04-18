016cba76e841c870f41bdb876e5ab95400958fab
Andrew C <acchang4@gmail.com>
2020-04-18 18:48:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,19 @@
 class Dog
 
-Dog.instance_variable_set(:@name)
-
-Dog.instance_variable_get(:@name)
-
+def name=(dog_name)
+  @name = dog_name
 end
 
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def name
-#     @name
-#   end
-# end
+def name
+  @name
+end
 
-# def name=(dog_name)
-#   @this_dogs_name = dog_name
-# end
-#
-# def name
-#   @this_dogs_name
-# end
-#
-# def breed=(dog_breed)
-#   @this_dogs_breed = dog_breed
-# end
-#
-# def breed
-#   @this_dogs_breed
-# end
-#
-# end
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+
+def breed
+  @breed
+end
+
+end
