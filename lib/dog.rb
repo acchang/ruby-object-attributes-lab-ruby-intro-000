364c2c0f6f6438325852e0e3a38d56f2dacda116
Andrew C <acchang4@gmail.com>
2020-04-18 18:42:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,28 @@
 class Dog
 
-def name=(dog_name)
-  @this_dogs_name = dog_name
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end 
 end
 
-def name
-  @this_dogs_name
-end
-
-def breed=(dog_breed)
-  @this_dogs_breed = dog_breed
-end
-
-def breed
-  @this_dogs_breed
-end
-
-end
+# def name=(dog_name)
+#   @this_dogs_name = dog_name
+# end
+#
+# def name
+#   @this_dogs_name
+# end
+#
+# def breed=(dog_breed)
+#   @this_dogs_breed = dog_breed
+# end
+#
+# def breed
+#   @this_dogs_breed
+# end
+#
+# end
